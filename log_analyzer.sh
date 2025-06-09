f40b72edb49d49cb1952a95cb28a3ae1dd219a74
marcosploit <114773938+marcosploit@users.noreply.github.com>
2025-06-09 16:09:03 -0300

    Update log_analyzer.sh
    Updated for color interpretation

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -15,10 +15,10 @@ validate_ip() {
     # Split the IP address into octets
     #IFS='.' read -r i1 i2 i3 i4 <<< "$ip"
     
-    i1=$(echo "$ip" | cut -d '.' -f 1)
-    i2=$(echo "$ip" | cut -d '.' -f 2)
-    i3=$(echo "$ip" | cut -d '.' -f 3)
-    i4=$(echo "$ip" | cut -d '.' -f 4)
+    i1=$(echo -e "$ip" | cut -d '.' -f 1)
+    i2=$(echo -e "$ip" | cut -d '.' -f 2)
+    i3=$(echo -e "$ip" | cut -d '.' -f 3)
+    i4=$(echo -e "$ip" | cut -d '.' -f 4)
     
     # Check if there are exactly 4 octets
     if [ "$i1" ] && [ "$i2" ] && [ "$i3" ] && [ "$i4" ]; then
@@ -34,31 +34,33 @@ validate_ip() {
     }
 
 while true; do
-    echo "==============================="
-    echo "    Apache Log file Analyzer"
-    echo "==============================="
-    echo "1. Define log file path"
-    echo "2. Show first and last lines of the file"
-    echo "3. Show first and last lines of the file of SPECIFIC IP"
-    echo "4. List involved IPs"
-    echo "5. Top 5 Involved IPs"
-    echo "6. Check for known hacker tools"
-    echo "7. Generate Full Report"
-    echo "8. Clear the screen"
-    echo "9. Exit"
-    echo "==============================="
+    echo -e "==============================="
+    echo -e "    Apache Log file Analyzer"
+    echo -e "==============================="
+    echo -e "1. Define log file path"
+    echo -e "2. Show first and last lines of the file"
+    echo -e "3. Show first and last lines of the file of SPECIFIC IP"
+    echo -e "4. List involved IPs"
+    echo -e "5. Top 5 Involved IPs"
+    echo -e "6. Check for known hacker tools"
+    echo -e "7. Generate Full Report"
+    echo -e "8. Clear the screen"
+    echo -e "9. Exit"
+    echo -e "==============================="
     read -p "Please select an option [1-9]: " option
 
     case $option in
         1)
             read -p "Enter new log path value: " new_path
             LOGPATH="$new_path"
-            echo "Path set to: $LOGPATH"
+            echo ""
+            echo -e "${YELLOW}Path set to: $LOGPATH ${NC}"
+            echo ""
             ;;
         2)
             #Check path
             if [ -z "$LOGPATH" ]; then
-                echo "Error: PATH variable is empty. Returning to the menu..."
+                echo -e "Error: PATH variable is empty. Returning to the menu..."
                 read -p "Press Enter to continue..." empty_arg
                 continue
             fi
@@ -66,25 +68,25 @@ while true; do
             
             
             #Operation
-            echo # Blank line for better readability
-            echo "==============================="
-            echo "${YELLOW}First 5 lines${NC}"
-            echo "==============================="
+            echo -e # Blank line for better readability
+            echo -e "==============================="
+            echo -e "${YELLOW}First 5 lines${NC}"
+            echo -e "==============================="
             head $LOGPATH -n5
-            echo
-            echo "==============================="
-            echo "${YELLOW}Last 5 lines${NC}"
-            echo "==============================="
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}Last 5 lines${NC}"
+            echo -e "==============================="
             tail $LOGPATH -n5
             
-            echo # Blank line for better readability
+            echo -e # Blank line for better readability
             read -p "Press Enter to return to the menu..." empty_arg
             ;;
 
 	3)
             #Check path
             if [ -z "$LOGPATH" ]; then
-                echo "Error: PATH variable is empty. Returning to the menu..."
+                echo -e "Error: PATH variable is empty. Returning to the menu..."
                 read -p "Press Enter to continue..." empty_arg
                 continue
             fi
@@ -93,72 +95,72 @@ while true; do
             while true; do
 		    read -p "Please enter a valid IP address: " ip_address
 		    if validate_ip "$ip_address"; then
-		       echo "The IP address $ip_address is valid."
+		       echo -e "The IP address $ip_address is valid."
 		       break
 		    else
-		       echo "Invalid IP address. Please try again."
+		       echo -e "Invalid IP address. Please try again."
 		    fi
             done
             
             #Operation
-            echo # Blank line for better readability
-            echo "==============================="
-            echo "${YELLOW}First 5 lines${NC} of $ip_address"
-            echo "==============================="
+            echo -e # Blank line for better readability
+            echo -e "==============================="
+            echo -e "${YELLOW}First 5 lines${NC} of $ip_address"
+            echo -e "==============================="
             cat $LOGPATH | grep $ip_address |head -n5
-            echo
-            echo "==============================="
-            echo "${YELLOW}Last 5 lines${NC} of $ip_address"
-            echo "==============================="
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}Last 5 lines${NC} of $ip_address"
+            echo -e "==============================="
             cat $LOGPATH | grep $ip_address |tail -n5
             
-            echo # Blank line for better readability
+            echo -e # Blank line for better readability
             read -p "Press Enter to return to the menu..." empty_arg
             ;;
         4)
             #Check path
             if [ -z "$LOGPATH" ]; then
-                echo "Error: PATH variable is empty. Returning to the menu..."
+                echo -e "Error: PATH variable is empty. Returning to the menu..."
                 read -p "Press Enter to continue..." empty_arg
                 continue
             fi
             
             #Operation
-            echo
-            echo "==============================="
-            echo "${YELLOW}IP addresses found${NC}"
-            echo "==============================="
-            echo
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}IP addresses found${NC}"
+            echo -e "==============================="
+            echo -e
             cat $LOGPATH | cut -d " " -f1 | sort | uniq -c | sort -unr
             
-            echo # Blank line for better readability
+            echo -e # Blank line for better readability
             read -p "Press Enter to return to the menu..." empty_arg
             ;;
             
         5)
             #Check path
             if [ -z "$LOGPATH" ]; then
-                echo "Error: PATH variable is empty. Returning to the menu..."
+                echo -e "Error: PATH variable is empty. Returning to the menu..."
                 read -p "Press Enter to continue..." empty_arg
                 continue
             fi
             
             #Operation
-            echo
-            echo "==============================="
-            echo "${YELLOW}Top 5 IP addresses${NC}"
-            echo "==============================="
-            echo
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}Top 5 IP addresses${NC}"
+            echo -e "==============================="
+            echo -e
             
             cat $LOGPATH | cut -d " " -f1 | sort | uniq -c | sort -unr | head -n5
             
-            echo # Blank line for better readability
+            echo -e # Blank line for better readability
             read -p "Press Enter to return to the menu..." empty_arg
             ;;
         6)
             #Check path
             if [ -z "$LOGPATH" ]; then
-                echo "Error: PATH variable is empty. Returning to the menu..."
+                echo -e "Error: PATH variable is empty. Returning to the menu..."
                 read -p "Press Enter to continue..." empty_arg
                 continue
             fi
@@ -166,129 +168,129 @@ while true; do
             #Operation
 
             
-            echo
-            echo "==============================="
-            echo "${YELLOW}Tools Identified in the Log file${NC}"
-            echo "==============================="
-            echo
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}Tools Identified in the Log file${NC}"
+            echo -e "==============================="
+            echo -e
             
             #Check Nmap
             if grep -iq "nmap"  $LOGPATH
             then 
-            	echo "${GREEN}NMAP USED${NC}";
+            	echo -e "${GREEN}NMAP USED${NC}";
             else
-                echo "${RED}NMAP probably NOT USED${NC}";
+                echo -e "${RED}NMAP probably NOT USED${NC}";
             fi
             
             #Check Nikto
             if grep -iq "nikto"  $LOGPATH
             then 
-            	echo "${GREEN}Nikto USED${NC}";
+            	echo -e "${GREEN}Nikto USED${NC}";
             else
-                echo "${RED}Nikto probably NOT USED${NC}";
+                echo -e "${RED}Nikto probably NOT USED${NC}";
             fi
             
             #Check Nessus
             if grep -iq "nessus"  $LOGPATH
             then 
-            	echo "${GREEN}Nessus USED${NC}";
+            	echo -e "${GREEN}Nessus USED${NC}";
             else
-                echo "${RED}Nessus probably NOT USED${NC}";
+                echo -e "${RED}Nessus probably NOT USED${NC}";
             fi
             
             #Check curl
             if grep -iq "curl/"  $LOGPATH
             then 
-            	echo "${GREEN}Requests via curl Identified${NC}";
+            	echo -e "${GREEN}Requests via curl Identified${NC}";
             else
-                echo "${RED}No requests via curl Identified${NC}";
+                echo -e "${RED}No requests via curl Identified${NC}";
             fi
             
-            echo # Blank line for better readability
+            echo -e # Blank line for better readability
             read -p "Press Enter to return to the menu..." empty_arg
             ;;
         7)
             #Check path
             if [ -z "$LOGPATH" ]; then
-                echo "Error: PATH variable is empty. Returning to the menu..."
+                echo -e "Error: PATH variable is empty. Returning to the menu..."
                 read -p "Press Enter to continue..." empty_arg
                 continue
             fi
             
             #Operation
                         
-            echo
-            echo "==============================="
-            echo "${YELLOW}Top 5 IP addresses${NC}"
-            echo "==============================="
-            echo
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}Top 5 IP addresses${NC}"
+            echo -e "==============================="
+            echo -e
             
             cat $LOGPATH | cut -d " " -f1 | sort | uniq -c | sort -unr | head -n5
             
             
             ip_address=$(cat $LOGPATH  | cut -d " " -f1 | sort | uniq -c | sort -unr | head -n1 | sed -e 's/^ *//;s/ /,/' | cut -d "," -f2)
             
-            echo # Blank line for better readability
-            echo "==============================="
-            echo "${YELLOW}First 5 lines${NC} of $ip_address"
-            echo "==============================="
+            echo -e # Blank line for better readability
+            echo -e "==============================="
+            echo -e "${YELLOW}First 5 lines${NC} of $ip_address"
+            echo -e "==============================="
             cat $LOGPATH | grep $ip_address |head -n5
-            echo
-            echo "==============================="
-            echo "${YELLOW}Last 5 lines${NC} of $ip_address"
-            echo "==============================="
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}Last 5 lines${NC} of $ip_address"
+            echo -e "==============================="
             cat $LOGPATH | grep $ip_address |tail -n5
             
-            echo
-            echo "==============================="
-            echo "${YELLOW}Tools Identified in the Log file${NC}"
-            echo "==============================="
-            echo
+            echo -e
+            echo -e "==============================="
+            echo -e "${YELLOW}Tools Identified in the Log file${NC}"
+            echo -e "==============================="
+            echo -e
             
             #Check Nmap
             if grep -iq "nmap"  $LOGPATH
             then 
-            	echo "${GREEN}NMAP USED${NC}";
+            	echo -e "${GREEN}NMAP USED${NC}";
             else
-                echo "${RED}NMAP probably NOT USED${NC}";
+                echo -e "${RED}NMAP probably NOT USED${NC}";
             fi
             
             #Check Nikto
             if grep -iq "nikto"  $LOGPATH
             then 
-            	echo "${GREEN}Nikto USED${NC}";
+            	echo -e "${GREEN}Nikto USED${NC}";
             else
-                echo "${RED}Nikto probably NOT USED${NC}";
+                echo -e "${RED}Nikto probably NOT USED${NC}";
             fi
             
             #Check Nessus
             if grep -iq "nessus"  $LOGPATH
             then 
-            	echo "${GREEN}Nessus USED${NC}";
+            	echo -e "${GREEN}Nessus USED${NC}";
             else
-                echo "${RED}Nessus probably NOT USED${NC}";
+                echo -e "${RED}Nessus probably NOT USED${NC}";
             fi
             
             #Check curl
             if grep -iq "curl/"  $LOGPATH
             then 
-            	echo "${GREEN}Requests via curl Identified${NC}";
+            	echo -e "${GREEN}Requests via curl Identified${NC}";
             else
-                echo "${RED}No requests via curl Identified${NC}";
+                echo -e "${RED}No requests via curl Identified${NC}";
             fi
-            echo
+            echo -e
             
             ;;
         8)
             clear
             ;;    
         9)
-            echo "Exiting the menu. Goodbye!"
+            echo -e "Exiting the menu. Goodbye!"
             exit 0
             ;;
         *)
-            echo "Invalid option. Please select a valid number [1-5]."
-            echo # Blank line for better readability
+            echo -e "Invalid option. Please select a valid number [1-5]."
+            echo -e # Blank line for better readability
             read -p "Press Enter to return to the menu..." empty_arg
             ;;
     esac
